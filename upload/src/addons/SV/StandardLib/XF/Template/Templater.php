543c28e37f4accb52114107d45c8753c08b1f91b
Xon <tazipper1@hotmail.com>
2020-08-10 10:16:53 +0800

    Extend Templater for adding replacevalue filter

---
+++ upload/src/addons/SV/StandardLib/XF/Template/Templater.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace SV\StandardLib\XF\Template;
+
+
+
+/**
+ * Extends \XF\Template\Templater
+ */
+class Templater extends XFCP_Templater
+{
+    public function addDefaultHandlers()
+    {
+        parent::addDefaultHandlers();
+
+        $hasFromCallable = is_callable('\Closure::fromCallable');
+
+        if (empty($this->filters['replacevalue']))
+        {
+            $callable = [$this, 'fnSvReplaceValue'];
+            if ($hasFromCallable)
+            {
+                /** @noinspection PhpElementIsNotAvailableInCurrentPhpVersionInspection */
+                $callable = \Closure::fromCallable($callable);
+            }
+            $this->addFilter('replacevalue', $callable);
+        }
+    }
+
+    /**
+     * @param Templater       $templater
+     * @param int[]|string[]  $value
+     * @param string          $escape
+     * @param int|string      $toReplace
+     * @param int|string|null $replaceWith
+     * @return int[]|string[]
+     * @noinspection PhpUnusedParameterInspection
+     */
+    public function fnReplaceValue($templater, $value, &$escape, $toReplace, $replaceWith)
+    {
+        foreach ($value as $key => $_val)
+        {
+            if ($_val === $toReplace)
+            {
+                if ($replaceWith === null)
+                {
+                    unset($value[$key]);
+                }
+                else
+                {
+                    $value[$key] = $replaceWith;
+                }
+            }
+        }
+
+        return $value;
+    }
+}
---
+++ upload/src/addons/SV/StandardLib/_output/extension_hint.php
@@ -0,0 +1,9 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE CLASS EXTENSIONS IN THE CONTROL PANEL.
+
+namespace SV\StandardLib\XF\Template
+{
+	class XFCP_Templater extends \XF\Template\Templater {}
+}
