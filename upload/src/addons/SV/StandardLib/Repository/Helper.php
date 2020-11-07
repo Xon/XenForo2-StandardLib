1c7d955a09060fba3d0e6776f7a18a9171796ff4
Xon <tazipper1@hotmail.com>
2020-11-08 03:50:19 +0800

    Add support in php for moment.js compatible time diff, and bundle moment.js with standardlib

--- upload/src/addons/SV/StandardLib/Repository/Helper.php
+++ upload/src/addons/SV/StandardLib/Repository/Helper.php
@@ -7,6 +7,160 @@ use \XF\Mvc\Entity\Repository;
 class Helper extends Repository
 {
     /**
+     * @param \DateInterval|array $interval
+     * @param int                 $continuousDateParts
+     * @param string              $phraseContext
+     * @return array
+     */
+    public function buildRelativeDateString($interval, int $continuousDateParts, string $phraseContext = 'raw'): array
+    {
+        if ($interval instanceof \DateInterval)
+        {
+            $interval = [
+                'y' => $interval->y,
+                'm' => $interval->m,
+                'd' => $interval->d,
+                'h' => $interval->h,
+                'i' => $interval->i,
+                's' => $interval->s,
+                'invert' => $interval->invert,
+            ];
+        }
+
+        $formatMaps = [
+            'y' => ['year', '%y '],
+            'm' => ['month', '%m '],
+            'd' => ['day', '%d '],
+            'h' => ['hour', '%h '],
+            'i' => ['minute', '%i '],
+            's' => ['second', '%s '],
+        ];
+
+        $dateArr = [];
+        $truncatingDate = $continuousDateParts > 0;
+        foreach ($formatMaps AS $format => $phrase)
+        {
+            if ($truncatingDate && \count($dateArr) === $continuousDateParts)
+            {
+                break;
+            }
+
+            $value = $interval[$format];
+            $len = count($dateArr);
+
+            if ($value === 1)
+            {
+                $dateArr[] = \XF::phrase('time.' . $phrase[0], [
+                    'count' => $value
+                ])->render($phraseContext);
+            }
+            else if ($value > 1)
+            {
+                $dateArr[] = \XF::phrase('time.' . $phrase[0] . 's', [
+                    'count' => $value
+                ])->render($phraseContext);
+            }
+
+            if ($truncatingDate && $len > 0 &&
+                ($len === count($dateArr) || count($dateArr) > $continuousDateParts))
+            {
+                break;
+            }
+        }
+
+        return $dateArr;
+    }
+
+    /**
+     * Generates an momentJs compatible date diff structure. Returns empty array on equal values
+     *
+     * @param int $a
+     * @param int $b
+     * @return int[]
+     */
+    public function momentJsCompatibleTimeDiff(int $a, int $b): array
+    {
+        $diffInSeconds = $b - $a;
+        if ($diffInSeconds === 0)
+        {
+            return [];
+        }
+        $data = ['invert' => $diffInSeconds < 0];
+
+        // Derived from momentJs, MIT licenced. For licence details see js/vendor/moment/LICENSE
+        // ***********
+
+        //$milliseconds = abs($diffInSeconds);
+        $seconds = abs($diffInSeconds);
+        $days = $months = $minutes = $hours = $years = $monthsFromDays = 0;
+        $absCeil = function ($number) {
+            if ($number < 0)
+            {
+                return floor($number);
+            }
+            else
+            {
+                return ceil($number);
+            }
+        };
+
+        $absFloor = function ($number) {
+            if ($number < 0)
+            {
+                // -0 -> 0
+                return ceil($number) || 0;
+            }
+            else
+            {
+                return floor($number);
+            }
+        };
+
+        $daysToMonths = function ($days) {
+            // 400 years have 146097 days (taking into account leap year rules)
+            // 400 years have 12 months === 4800
+            return ($days * 4800) / 146097;
+        };
+
+        $monthsToDays = function ($months) {
+            // the reverse of daysToMonths
+            return ($months * 146097) / 4800;
+        };
+
+        // The following code bubbles up values, see the tests for
+        // examples of what that means.
+//        $data['f'] = (int)($milliseconds % 1000);
+
+//        $seconds = $absFloor($milliseconds / 1000);
+        $data['s'] = (int)($seconds % 60);
+
+        $minutes = $absFloor($seconds / 60);
+        $data['i'] = (int)($minutes % 60);
+
+        $hours = $absFloor($minutes / 60);
+        $data['h'] = (int)($hours % 24);
+
+        $days = $absFloor($hours / 24);
+
+        // convert days to months
+        $monthsFromDays = $absFloor($daysToMonths($days));
+        $months += $monthsFromDays;
+        $days -= $absCeil($monthsToDays($monthsFromDays));
+
+        // 12 months -> 1 year
+        $years = $absFloor($months / 12);
+        $months %= 12;
+
+        $data['d'] = (int)$days;
+        $data['m'] = (int)$months;
+        $data['y'] = (int)$years;
+
+        // ***********
+
+        return $data;
+    }
+
+    /**
      * @param mixed|\XF\Mvc\Entity\Entity|null $entity
      * @param string                           $relationOrGetter
      * @param string                           $backupColumn
