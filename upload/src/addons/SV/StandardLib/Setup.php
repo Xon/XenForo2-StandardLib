5e2a85696a2e10fa3f1006cf548f9dbdc8a1b611
Xon <tazipper1@hotmail.com>
2020-11-08 22:17:04 +0800

    On uninstall, de-own the various time.* phrases

---
+++ upload/src/addons/SV/StandardLib/Setup.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace SV\StandardLib;
+
+use XF\AddOn\AbstractSetup;
+use XF\AddOn\StepRunnerInstallTrait;
+use XF\AddOn\StepRunnerUninstallTrait;
+use XF\AddOn\StepRunnerUpgradeTrait;
+
+class Setup extends AbstractSetup
+{
+    use InstallerHelper;
+    use StepRunnerInstallTrait;
+    use StepRunnerUpgradeTrait;
+    use StepRunnerUninstallTrait;
+
+    public function uninstallStep1()
+    {
+        /** @var \XF\Entity\Phrase[] $phrases */
+        $phrases = \XF::finder('XF:Phrase')
+                      ->where('language_id', '=', 0)
+                      ->where('addon_id', '=', 'SV/StandardLib')
+                      ->where('title', 'like', 'time.%')
+                      ->fetch();
+        foreach ($phrases as $phrase)
+        {
+            $phrase->addon_id = '';
+            $phrase->saveIfChanged();
+        }
+    }
+}
