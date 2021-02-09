565159ed966958ddf5ed376e44ff4011a3589f35
ticktackk <me@ticktackk.io>
2021-02-09 05:31:12 +0530

    Add extension for `XF.Tabs` to allow storing selected tab id in hidden input

---
+++ upload/src/addons/SV/StandardLib/_files/js/sv/lib/xf/core/structure.js
@@ -0,0 +1,63 @@
+// noinspection JSUnusedLocalSymbols
+(function ($, window, document, _undefined)
+{
+    "use strict";
+
+    XF.Tabs = XF.extend(XF.Tabs, {
+        __backup: {
+            'activateTab': 'svStandardLib_activateTab'
+        },
+
+        options: $.extend({}, XF.Tabs.prototype.options, {
+            svStoreSelectedTab: false,
+            svStoreSelectedTabInputName: null
+        }),
+
+        activateTab: function(offset)
+        {
+            this.svStandardLib_activateTab(offset);
+
+            this.handleStoringOfSelectedTabIfNeeded(offset);
+        },
+
+        _getHiddenInput: function ()
+        {
+            var $form = this.$target.closest('form'),
+                finalInputSelector = '[name="' + XF.htmlspecialchars(this.options.svStoreSelectedTabInputName.toString()) + '"]',
+                $hiddenInput = XF.findRelativeIf(finalInputSelector, $form);
+
+            if (!$hiddenInput.length)
+            {
+                $('<input />', {
+                    type: 'hidden',
+                    name: XF.htmlspecialchars(this.options.svStoreSelectedTabInputName),
+                    value: ''
+                }).appendTo($form);
+            }
+
+            return XF.findRelativeIf(finalInputSelector, $form);
+        },
+
+        /**
+         * @param {Number} offset
+         */
+        handleStoringOfSelectedTabIfNeeded: function (offset)
+        {
+            if (!this.options.svStoreSelectedTab || !this.options.svStoreSelectedTabInputName)
+            {
+                return;
+            }
+
+            var $tab = this.$tabs.eq(offset),
+                $pane = this.$panes.eq(offset);
+            if (!$tab.length || !$pane.length)
+            {
+                return;
+            }
+
+            var selectedTab = $tab.is('[id]') ? $tab.attr('id') : '';
+
+            this._getHiddenInput().val(selectedTab);
+        }
+    });
+} (jQuery, window, document));
