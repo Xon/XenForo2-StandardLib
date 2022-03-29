21342a3592098d52c73c619c7f37b8e13baf0f67
Xon <tazipper1@hotmail.com>
2022-03-30 04:28:57 +0800

    Add cache rebuild behaviour

---
+++ upload/src/addons/SV/StandardLib/Behavior/Cacheable.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace SV\StandardLib\Behavior;
+
+use XF\Mvc\Entity\Behavior;
+
+class Cacheable extends Behavior
+{
+    protected function getDefaultConfig(): array
+    {
+        return [
+            'repository' => $this->entity->structure()->shortName,
+            'rebuildCacheFuncName' => 'rebuildCache',
+            'shouldRebuildCallable' => null,
+        ];
+    }
+
+    public function postSave()
+	{
+        if ($this->shouldRebuild())
+        {
+            $this->rebuildCache();
+        }
+	}
+
+	public function postDelete()
+	{
+        if ($this->shouldRebuild())
+        {
+            $this->rebuildCache();
+        }
+	}
+
+    protected function shouldRebuild(): bool
+    {
+        $func = $this->config['shouldRebuildCallable'];
+        if ($func !== null && \is_callable($func))
+        {
+            return $func($this->entity);
+        }
+
+        return true;
+    }
+	
+	public function rebuildCache()
+	{
+        $class = $this->config['repository'];
+        $func = $this->config['rebuildCacheFuncName'];
+
+        $repo = $this->repository($class);
+        $callable = [$repo, $func];
+        if (\is_callable($callable))
+        {
+            $callable();
+        }
+        else if (\XF::$developmentMode || \XF::$debugMode)
+        {
+            $e = new \LogicException('Expected '.$class.'::'.$func.' method to exist');
+            if (\XF::$developmentMode)
+            {
+                throw $e;
+            }
+            else
+            {
+                \XF::logException($e);
+            }
+        }
+	}
+}
