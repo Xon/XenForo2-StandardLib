ab4d01c27bc52b7c51f4c0349bfd4b5e68610081
Xon <tazipper1@hotmail.com>
2022-10-17 23:58:21 +0800

    Work-around for people who badly update XF add-on sites

---
+++ upload/src/addons/SV/StandardLib/XF/Template/Templater.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @noinspection PhpUndefinedClassInspection
+ */
+
+namespace SV\StandardLib\XF\Template;
+
+class Templater extends XFCP_Templater
+{
+    // This is an empty file to handle the case where people delete the entire add-on files, upload the new files, and then wonder why their entire site breaks as XenForo still tries to load this file.
+}
