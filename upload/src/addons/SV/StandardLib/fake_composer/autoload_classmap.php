f9bcc9180b48ad0a02ea2a0ae9237776cef79e5e
Xon <635541+Xon@users.noreply.github.com>
2024-04-30 23:15:38 +0800

    Backport XF2.2 LinkableInterface

--- upload/src/addons/SV/StandardLib/fake_composer/autoload_classmap.php
+++ upload/src/addons/SV/StandardLib/fake_composer/autoload_classmap.php
@@ -1,11 +1,17 @@
 <?php
-/** @noinspection PhpFullyQualifiedNameUsageInspection */
+/**
+ * @noinspection PhpUnnecessaryFullyQualifiedNameInspection
+ * @noinspection PhpFullyQualifiedNameUsageInspection
+ */
 
+$classmap  = [];
 if (\XF::$versionId < 2030036)
 {
-    return [
-        'XF\Entity\ViewableInterface' => __DIR__ . '/shim/ViewableInterface.php',
-    ];
+    $classmap[\XF\Entity\ViewableInterface::class] = __DIR__ . '/shim/ViewableInterface.php';
+}
+if (\XF::$versionId < 2020031)
+{
+    $classmap[\XF\Entity\LinkableInterface::class] = __DIR__ . '/shim/LinkableInterface.php';
 }
 
-return [];
+return $classmap;
---
+++ upload/src/addons/SV/StandardLib/fake_composer/shim/LinkableInterface.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * @noinspection PhpMissingReturnTypeInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+namespace XF\Entity;
+
+interface LinkableInterface
+{
+    /**
+     * @param bool  $canonical
+     * @param array $extraParams
+     * @param string|null $hash
+     * @return string
+     */
+    public function getContentUrl(bool $canonical = false, array $extraParams = [], $hash = null);
+
+    /**
+     * @return string|null
+     */
+    public function getContentPublicRoute();
+
+    /**
+     * @param string $context
+     * @return string|\XF\Phrase
+     */
+    public function getContentTitle(string $context = '');
+}
