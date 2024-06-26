86f715b91cfabe7500859268ca8564cfa748bffe
Xon <635541+Xon@users.noreply.github.com>
2024-06-27 00:06:15 +0800

    Fix XF2.1 support for <xf:macro name="template::macro" syntax

---
+++ upload/src/addons/SV/StandardLib/XF/Template/TemplaterXF21Patch.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace SV\StandardLib\XF\Template;
+
+use function count;
+use function explode;
+
+if (\XF::$versionId < 2012000)
+{
+    class TemplaterXF21Patch extends XFCP_TemplaterXF21Patch
+    {
+        public function callMacro($template, $name, array $arguments, array $globalVars)
+        {
+            if (\XF::$versionId <= 2020000)
+            {
+                if (!$template)
+                {
+                    $nameParts = explode('::', $name, 2);
+                    if (count($nameParts) == 2)
+                    {
+                        $template = $nameParts[0];
+                        $name = $nameParts[1];
+                    }
+                }
+            }
+
+            return parent::callMacro($template, $name, $arguments, $globalVars);
+        }
+    }
+}
+else
+{
+    class TemplaterXF21Patch extends XFCP_TemplaterXF21Patch {}
+}
