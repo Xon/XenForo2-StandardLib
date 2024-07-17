9cc9c1a1e18a88f4ec71a61c0cd6e5efcbeb9784
TickTackk <me@ticktackk.io>
2024-07-17 19:13:50 +0530

    WIP: Add support for enabling/disabling based on `XF.Disabler()`
    (cherry picked from commit b7a1d6a3842b52771abcedb28a098f2bb6cfce6a)

--- upload/src/addons/SV/StandardLib/_files/js/sv/lib/select.js
+++ upload/src/addons/SV/StandardLib/_files/js/sv/lib/select.js
@@ -110,6 +110,8 @@ SV.extendObject = SV.extendObject || XF.extendObject || jQuery.extend;
                 $target.on('choice', this.onChoice.bind(this));
                 $target.on('showDropdown', this.onShowDropdown.bind(this));
                 $target.on('hideDropdown', this.onHideDropdown.bind(this));
+                $target.on('control:enabled', this.onControlEnabled.bind(this));
+                $target.on('control:disabled', this.onControlDisabled.bind(this));
             }
             else
             {
@@ -118,6 +120,8 @@ SV.extendObject = SV.extendObject || XF.extendObject || jQuery.extend;
                 XF.on(passedElement, 'choice', this.onChoice.bind(this));
                 XF.on(passedElement, 'showDropdown', this.onShowDropdown.bind(this));
                 XF.on(passedElement, 'hideDropdown', this.onHideDropdown.bind(this));
+                XF.on(passedElement, 'control:enabled', this.onControlEnabled.bind(this));
+                XF.on(passedElement, 'control:disabled', this.onControlDisabled.bind(this));
             }
         },
 
@@ -166,7 +170,124 @@ SV.extendObject = SV.extendObject || XF.extendObject || jQuery.extend;
         onHideDropdown: function (event)
         {
         },
+
+        onControlEnabled: function (event)
+        {
+            if (!this.choices)
+            {
+                console.error('Choices not setup.')
+                return
+            }
+
+            this.choices.enable()
+        },
+
+        onControlDisabled: function (event)
+        {
+            if (!this.choices)
+            {
+                console.error('Choices not setup.')
+                return
+            }
+
+            this.choices.disable()
+        },
     });
 
-    XF.Element.register('sv-choices', 'SV.StandardLib.Choices');
+    SV.StandardLib.ChoicesLoader = XF.Element.newHandler({
+        options: {
+            listenTo: '',
+            initUpdate: true,
+            href: true
+        },
+
+        init ()
+        {
+            let theTarget = this.target || this.$target.get(0)
+            if (!theTarget.matches('select'))
+            {
+                console.error('Must trigger on select')
+            }
+
+            if (this.options.href)
+            {
+                const listenTo = this.options.listenTo ? XF.findRelativeIf(this.options.listenTo, this.target || this.$target) : null
+                if (!listenTo)
+                {
+                    console.error('Cannot load choices dynamically as no element set to listen to for changes')
+                }
+                else
+                {
+                    if (typeof XF.on !== "function") // XF 2.2
+                    {
+                        listenTo.on('change', XF.proxy(this, 'loadChoices'));
+                    }
+                    else
+                    {
+                        XF.on(listenTo, 'change', this.loadChoices.bind(this));
+                    }
+
+                    if (this.options.initUpdate)
+                    {
+                        if (typeof XF.trigger === "function")
+                        {
+                            XF.trigger(listenTo, 'change');
+                        }
+                        else
+                        {
+                            listenTo.trigger('change');
+                        }
+                    }
+                }
+            }
+        },
+
+        loadChoices (e)
+        {
+            XF.ajax('POST', this.options.href, {
+                val: e.target.value
+            }, this.loadSuccess.bind(this))
+        },
+
+        loadSuccess (data)
+        {
+            if (data.html)
+            {
+                const select = this.target || this.$target.get(0)
+
+                // @todo: XF 2.3 compatibility
+                XF.setupHtmlInsert(data.html, html =>
+                {
+                    const val = select.value
+
+                    if (html instanceof HTMLSelectElement)
+                    {
+                        select.innerHTML = ''
+                        select.append(...Array.from(html.children))
+
+                        let hasValue = false
+                        const options = select.querySelectorAll('option')
+
+                        options.forEach((option) =>
+                        {
+                            if (option.value === val)
+                            {
+                                select.value = val
+                                hasValue = true
+                            }
+                        })
+                        if (!hasValue)
+                        {
+                            select.value = options[0].value
+                        }
+
+                        return true
+                    }
+                })
+            }
+        }
+    })
+
+    XF.Element.register('sv-choices', 'SV.StandardLib.Choices')
+    XF.Element.register('sv-choices-loader', 'SV.StandardLib.ChoicesLoader')
 }) ();
