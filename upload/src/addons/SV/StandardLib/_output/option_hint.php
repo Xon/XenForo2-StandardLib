6b15a58b1e3dee0cdc68c0c175f2f90283ddc17c
Xon <635541+Xon@users.noreply.github.com>
2024-11-13 00:34:43 +0800

    XF2.3+ export

---
+++ upload/src/addons/SV/StandardLib/_output/option_hint.php
@@ -0,0 +1,18 @@
+<?php
+
+// ################## THIS IS A GENERATED FILE ##################
+// DO NOT EDIT DIRECTLY. EDIT THE OPTIONS IN THE CONTROL PANEL.
+
+/**
+ * @noinspection PhpMultipleClassDeclarationsInspection
+ * @noinspection PhpIllegalPsrClassPathInspection
+ */
+
+namespace XF;
+
+/**
+ * @property bool|null $svLogLessFunc Standard Lib: Log parse_less_func debug info
+ */
+class Options
+{
+}
